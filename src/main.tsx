8520addc5ab8f807799f6ad3264a9c0fa19a89d9
Taj-786 <tajuddinshaik786r@gmail.com>
2025-03-16 00:52:42 +0530

    Stars background

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import StarsCanvas from './components/main/StarBackground'
 import './styles.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <StarsCanvas />
     <App />
   </StrictMode>,
 )
