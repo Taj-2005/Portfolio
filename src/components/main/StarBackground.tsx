f1e17140a43e03d354e08cd46363ff8b2ff82520
Taj-786 <tajuddinshaik786r@gmail.com>
2025-03-16 00:57:01 +0530

    Stars background

--- src/components/main/StarBackground.tsx
+++ src/components/main/StarBackground.tsx
@@ -2,7 +2,7 @@
 
 import {useState, useRef, Suspense} from 'react'
 import {Canvas, useFrame} from '@react-three/fiber'
-import {Points, PointMaterial, Preload} from '@react-three/drei'
+import {Points, PointMaterial} from '@react-three/drei'
 // @ts-ignore
 import * as random from 'maath/random/dist/maath-random.esm'
 
@@ -10,7 +10,7 @@ const StarBackground = (props: any) => {
   const ref : any = useRef(null)
   const [sphere] = useState(() => random.inSphere(new Float32Array(5000), {radius : 1.2}))
 
-  useFrame((state, delta ) => {
+  useFrame((_, delta ) => {
     if (ref.current) { 
       ref.current.rotation.x -= delta/10
       ref.current.rotation.y -= delta/15 
