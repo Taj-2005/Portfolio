7e841662d4aa5355a19f11208e6c9cc0fef00a35
Taj-786 <tajuddinshaik786r@gmail.com>
2025-03-16 03:50:36 +0530

    Projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import Encryption from './components/main/Encryption'
 import Hero from './components/main/Hero'
 import Navbar from './components/main/Navbar'
+import Projects from './components/main/Projects'
 import Skills from './components/main/Skills'
 import './styles.css'
 
@@ -12,6 +14,8 @@ function App() {
        <div className='flex flex-col  gap-20'>
         <Hero />
         <Skills />
+        <Encryption />
+        <Projects />
        </div>
     </div>
   )
---
+++ src/components/main/Projects.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import ProjectCard from "../sub/ProjectCard"
+import nextWeb from '../../../public/NextWebsite.png'
+import cardImg from '../../../public/CardImage.png'
+import space from '../../../public/SpaceWebsite.png'
+
+
+const Projects = () => {
+  return (
+    <div className="flex flex-col items-center justify-center py-20">
+        <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+            My Projects
+        </h1>
+        <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+            <ProjectCard 
+            src={nextWeb}
+            title="Next Website"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet obcaecati reprehenderit alias amet quisquam dolore fuga quibusdam libero porro perferendis non inventore enim quo, voluptates repudiandae vero nulla, officia aut?"
+            />
+
+            <ProjectCard 
+            src={cardImg}
+            title="Card Image"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet obcaecati reprehenderit alias amet quisquam dolore fuga quibusdam libero porro perferendis non inventore enim quo, voluptates repudiandae vero nulla, officia aut?"
+            />
+
+            <ProjectCard 
+            src={space}
+            title="Space Website"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet obcaecati reprehenderit alias amet quisquam dolore fuga quibusdam libero porro perferendis non inventore enim quo, voluptates repudiandae vero nulla, officia aut?"
+            />
+        </div>
+    </div>
+  )
+}
+
+export default Projects
